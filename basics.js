c48a62d0de196267bfb3fc2f55331822a11d143e
lahiru <lahiruthennakoon@gmail.com>
2024-12-17 00:31:57 +0530

    add code lines

---
+++ basics.js
@@ -0,0 +1,13 @@
+/*Declare a variable using var, let, and const. What
+ happens if you try to reassign a value to a const
+ variable?
+ (Print the result*/
+
+ var total = 15;
+ let name = "dumidu"
+ const pi = 5.21
+
+ console.log(typeof(total))
+ console.log(typeof(name))
+ console.log(typeof(pi))
+ 
